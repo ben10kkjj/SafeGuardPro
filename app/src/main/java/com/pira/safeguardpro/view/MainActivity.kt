e33059d3efae3a8e4447b178fd80b76b0c4e2b4d
Rakel Laiane Oliveira da Silva <rakel.silva@portalsesisp.org.br>
2024-05-13 08:19:19 -0300

    alinhei os elementos e nomeei os ids

--- app/src/main/java/com/pira/safeguardpro/view/MainActivity.kt
+++ app/src/main/java/com/pira/safeguardpro/view/MainActivity.kt
@@ -2,11 +2,34 @@ package com.pira.safeguardpro.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.pira.safeguardpro.R
+import com.pira.safeguardpro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //Criar a toolbar
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    //Criar a navegação
+    private lateinit var navController: NavController
+
+    //Criar o binding
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //Configura o binding
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Configura a navegação e a toolbar
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(binding.fragmentContainerView.id) as NavHostFragment
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 }
